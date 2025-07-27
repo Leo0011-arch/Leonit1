1dc615bd9c6ef5befdfd3db2651e5928e3b0664e
Leo0011-arch <ajdjaliii989@gmail.com>
2025-07-27 07:10:10 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -145,4 +145,4 @@ async def change_color():
         except Exception as e:
             print("❌ Error në change_color:", e)
 
-bot.run("TOKEN")
+bot.run("MTM5ODgwODM4MDgzOTE3MDA1OA.GovTtk.MIH2NYAtBLiySe7bUK2P_7IkMwKDyeZnQgahZk")
